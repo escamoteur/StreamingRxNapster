d13f696e1178db6deb91eded98ffb72a1931b8f5
escamoteur <thomas@burkharts.net>
2016-09-22 14:10:07 +0200

    Add project files.

---
+++ StreamingRxNapster/StreamingRxNapster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace StreamingRxNapster
+{
+    public class StreamingRxNapster : ContentPage
+    {
+        public StreamingRxNapster()
+        {
+            var button = new Button
+            {
+                Text = "Click Me!",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            int clicked = 0;
+            button.Clicked += (s, e) => button.Text = "Clicked: " + clicked++;
+
+            Content = button;
+        }
+    }
+}
