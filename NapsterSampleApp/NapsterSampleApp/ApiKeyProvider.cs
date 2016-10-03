0afff9e6db37ceabfe5b9eb350620da2ef43a26a
escamoteur <thomas@burkharts.net>
2016-10-03 18:03:39 +0200

    Added Napster DemoKey

--- NapsterSampleApp/NapsterSampleApp/ApiKeyProvider.cs
+++ NapsterSampleApp/NapsterSampleApp/ApiKeyProvider.cs
@@ -9,7 +9,10 @@ namespace NapsterSampleApp
     //Don't push this file to GitHub if you inserted your real keys
     public static class ApiKeyProvider
     {
-        public static string GetApiKey => "yourKey";
-        public static string GetApiSecret => "yourSecret";
+        // This is a Demokey from Napster that should not be used in production. Just register your own App with Napster
+        public static string GetApiKey => "NmE0ZTY0MTUtZDdkMy00MDkxLWFhN2YtMjdlZmY5MTljN2I5";
+        public static string GetApiSecret => "MzBkNmY4NzYtNjhlNy00MDY5LTk3YjUtNjFiYjMwY2YwYjRh";
     }
+
+
 }
